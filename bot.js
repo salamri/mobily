a1b494fceafd2dd8684fb2d1b75ae5bca61f843d
salamri <42224359+salamri@users.noreply.github.com>
2018-08-13 02:50:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const prefix = '$';
+const prefix = '$$';
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
