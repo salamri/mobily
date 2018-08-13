cd20a0d5ffdf1ee6d377cad048774c6f76b11fd7
salamri <42224359+salamri@users.noreply.github.com>
2018-08-13 12:10:41 +0300

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -18,6 +18,33 @@ client.on('message', msg => {
 
 
 
+client.on("message", message => {
+    const prefix = "!"
+              
+          if(!message.channel.guild) return;
+   if(message.author.bot) return;
+      if(message.content === prefix + "image"){ 
+          const embed = new Discord.RichEmbed()
+  
+      .setTitle(`This is  ** ${message.guild.name} **  Photo !`)
+  .setAuthor(message.author.username, message.guild.iconrURL)
+    .setColor(0x164fe3)
+    .setImage(message.guild.iconURL)
+    .setURL(message.guild.iconrURL)
+                    .setTimestamp()
+
+   message.channel.send({embed});
+      }
+  });
+
+
+
+
+
+
+
+
+
 client.on('message', msg => {
     if(msg.author.bot) return;
     
