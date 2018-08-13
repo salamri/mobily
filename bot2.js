a48ae03a382b031e4f1b81f76a8d78f07fdc5264
salamri <42224359+salamri@users.noreply.github.com>
2018-08-13 04:01:08 +0300

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -16,6 +16,34 @@ client.on('message', msg => {
 
 
 
+client.on('message',message =>{
+    var prefix = "!";
+    if(message.content.startsWith(prefix + 'top')) {
+  message.guild.fetchInvites().then(i =>{
+  var invites = [];
+   
+  i.forEach(inv =>{
+    var [invs,i]=[{},null];
+     
+    if(inv.maxUses){
+        invs[inv.code] =+ inv.uses+"/"+inv.maxUses;
+    }else{
+        invs[inv.code] =+ inv.uses;
+    }
+        invites.push(`invite: ${inv.url} inviter: ${inv.inviter} \`${invs[inv.code]}\`;`);
+   
+  });
+  var embed = new Discord.RichEmbed()
+  .setColor("#000000")
+  .setDescription(`${invites.join(`\n`)+'\n\n**By:** '+message.author}`)
+  .setThumbnail("https://i.imgur.com/OM00xyh.png")
+           message.channel.send({ embed: embed });
+   
+  });
+   
+    }
+  });
+
 
 
 
