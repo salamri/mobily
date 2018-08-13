ff7a9622eedd37b043654447fa4de7a501ececfb
salamri <42224359+salamri@users.noreply.github.com>
2018-08-13 04:09:39 +0300

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -14,23 +14,6 @@ client.on('message', msg => {
 
 
 
-```
-client.on('message', message => {
-    var args = message.content.split(/[ ]+/)
-    if(message.content.includes('السبة')){
-        message.delete()
-      message.channel.sendMessage("", {embed: {
-        title: "لا تسب",
-        color: 0x06DF00,
-        description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
-        footer: {
-          text: "By Abo Khalil"
-        }
-      }}).then(msg => {msg.delete(4000)});
-                          }
-
-     
-}); 
 
 
 
