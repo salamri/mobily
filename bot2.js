0c54894fbc5d70525a70825a34514218b8b065e6
salamri <42224359+salamri@users.noreply.github.com>
2018-08-13 04:30:40 +0300

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -16,6 +16,65 @@ client.on('message', msg => {
 
 
 
+
+
+
+const codes = {
+    ' ': '   ',
+    '0': '0⃣',
+    '1': '1⃣',
+    '2': '2⃣',
+    '3': '3⃣',
+    '4': '4⃣',
+    '5': '5⃣',
+    '6': '6⃣',
+    '7': '7⃣',
+    '8': '8⃣',
+    '9': '9⃣',
+    '!': '❕',
+    '?': '❔',
+    '#': '#⃣',
+    '*': '*⃣'
+  };
+  
+  'abcdefghijklmnopqrstuvwxyz'.split('').forEach(c => {
+    codes[c] = codes[c.toUpperCase()] = ` :regional_indicator_${c}:`;
+  });
+  
+  
+  client.on('message' , async message => {
+      var prefix = "-";
+         if(message.content.startsWith(prefix ! "emoji")) {
+            let args = message.content.split(" ").slice(1);
+    if (args.length < 1) {
+      message.channel.send('You must provide some text to emojify!');
+  }
+  
+  message.channel.send(
+      args.join(' ')
+          .split('')
+          .map(c => codes[c] || c)
+          .join('')
+  );
+  };
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 client.on('message', message => {
  if (message.content.startsWith("ترحيب 1")) {
                                  var mentionned = message.mentions.users.first();
