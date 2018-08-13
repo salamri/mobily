2684f639af47017a6325ced881eb74e6cc8b323e
salamri <42224359+salamri@users.noreply.github.com>
2018-08-13 08:32:16 +0300

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -19,6 +19,47 @@ client.on('message', msg => {
 
 
 
+client.on('message',function(message) {
+  if(!message.channel.guild) return;
+
+const prefix = "!";
+    if (message.content === prefix + "discrim") {
+let messageArray = message.content.split(" ");
+let args = messageArray.slice(1);
+
+if (message.author.bot) return;
+
+var discri = args[0]
+let discrim
+if(discri){
+discrim = discri;
+}else{
+discrim = message.author.discriminator;
+}
+if(discrim.length == 1){
+discrim = "000"+discrim
+}
+if(discrim.length == 2){
+discrim = "00"+discrim
+}
+if(discrim.length == 3){
+discrim = "0"+discrim
+}
+
+const users = client.users.filter(user => user.discriminator === discrim).map(user => user.username);
+return message.channel.send(`
+**Found ${users.length} users with the discriminator #${discrim}**
+${users.join('\n')}
+`);
+}
+});
+
+
+
+
+
+
+
 
 
 
