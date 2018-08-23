f49ec2aafb753cd033e43e03a38c926ba88e60cd
salamri <42224359+salamri@users.noreply.github.com>
2018-08-23 21:33:21 +0300

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -18,6 +18,15 @@ client.on('message', msg => {
 
 
 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
+:crown:اسم العضو  ${member}:crown:  
+انت العضو رقم ${member.guild.memberCount} `) 
+}).catch(console.error)
+})
+
+
 
 
 
