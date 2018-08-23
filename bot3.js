1b5483994ed32717597bca1cfc1461a6cdbb21e2
salamri <42224359+salamri@users.noreply.github.com>
2018-08-23 20:44:45 +0300

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -21,50 +21,6 @@ client.on('message', msg => {
 
 
 
-client.on('message', message => { 
-    var p = "*"
-        if (message.content.startsWith(p + `alpha`)) {
-message.guild.setName(`.a`)
-.then(msg => {
- setInterval(() => {
-message.guild.setName(`.al`)
-.then(msg => {
- setInterval(() => {
-message.guild.setName(`.alp`) 
-.then(msg => {
- setInterval(() => {
-message.guild.setName(`.alph`)
-.then(msg => {
- setInterval(() => {
-message.guild.setName(`.alpha`)
-.then(msg => {
- setInterval(() => {
-message.guild.setName(`.alpha `)
-.then(msg => {
- setInterval(() => {
-}, 11000);
-});
-},  10000);
-});
-},  8000);
-});
-},  6000);
-});
-},  4000);
-});
-},  1000);
-})
-        .catch(console.error);
-  //message.delete().catch(O_o=>{}); 
- 
-}
-
-});
-
-
-
-
-
 
 
 
