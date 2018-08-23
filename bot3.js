b72493cbce4ee5923ed944ecab0301f421f5ccee
salamri <42224359+salamri@users.noreply.github.com>
2018-08-23 21:46:42 +0300

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -23,41 +23,6 @@ client.on('message', msg => {
 
 
 
-const d = require("discord.js");
-const client = new d.Client();
-var prefix ="-"//:3 غيره بكيفك
-var colors = new Array(100);
-
-client.on("message", (message) => {
-    var command = message.content.split(" ")[0].slice(prefix.length);
-    switch(command) {
-        case "create-colors" :
-        if (!message.channel.type == "text") return;
-        if (!message.member.hasPermission("MANAGE_ROLES")) return
-        for(var x = 0;x<colors.length;x++){
-            message.guild.createRole({name : x, color : colors[x]});
-        };
-    };
-}).login("process.env.BOT_TOKEN3");
-function sin_to_hex(i, phase) {
-    var sin = Math.sin(Math.PI / size * 2 * i + phase);
-    var int = Math.floor(sin * 127) + 128;
-    var hex = int.toString(16);
-
-    return hex.length === 1 ? '0'+hex : hex;
-};
-for(var x=0;x<colors.length;x++) {
-    let r   = sin_to_hex(i, 0 * Math.PI * 2/3);
-    let b  = sin_to_hex(i, 1 * Math.PI * 2/3);
-    let g = sin_to_hex(i, 2 * Math.PI * 2/3);
-    colors[x] = '#'+ r + g + b;
-};
-
-
-
-
-
-
 
 client.on('message', message => {
      if (message.author.bot) return;
@@ -83,43 +48,6 @@ client.on('message', message => {
     .setDescription(" مدة الرابط : ساعه  عدد استخدامات الرابط : 1 ")
       message.author.sendEmbed(Embed11)
     }
-
-
-
-
-
-client.on('message', message => {
-    if (message.content.startsWith("!bot")) {
-    message.channel.send({
-        embed: new Discord.RichEmbed()
-            .setAuthor(client.user.username,client.user.avatarURL)
-            .setThumbnail(client.user.avatarURL)
-            .setColor('RANDOM')
-            .setTitle('``INFO Morro Bot`` ')
-            .addField('``My Ping``' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
-            .addField('``RAM Usage``', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
-            .addField('``servers``', [client.guilds.size], true)
-            .addField('``channels``' , `[ ${client.channels.size} ]` , true)
-            .addField('``Users``' ,`[ ${client.users.size} ]` , true)
-            .addField('``My Name``' , `[ ${client.user.tag} ]` , true)
-            .addField('``My ID``' , `[ ${client.user.id} ]` , true)
-                  .addField('``My Prefix``' , `[ ! ]` , true)
-                  .addField('``My Language``' , `[ Java Script ]` , true)
-                  .setFooter('By | MHSTR')
-    })
-}
-});
-
-
-
-
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
-:crown:اسم العضو  ${member}:crown:  
-انت العضو رقم ${member.guild.memberCount} `) 
-}).catch(console.error)
-})
 
 
 
