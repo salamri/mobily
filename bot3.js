171668835745f0e222b0704c5cd92cb991a39af2
salamri <42224359+salamri@users.noreply.github.com>
2018-08-23 21:38:57 +0300

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -15,6 +15,40 @@ client.on('message', msg => {
 
 
 
+
+client.on('message', message => {
+     if (message.author.bot) return;
+    if (message.content.startsWith("رابط")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 1,
+        maxAge: 3600,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription(" تم أرسال الرابط برسالة خاصة ")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription(" مدة الرابط : ساعه  عدد استخدامات الرابط : 1 ")
+      message.author.sendEmbed(Embed11)
+    }
+
+
+
+
+
+
+
+
+
 client.on('message', message => {
     if (message.content.startsWith("!bot")) {
     message.channel.send({
