5292ab44806188c0ace2a7af4105620c9a2eb39d
salamri <42224359+salamri@users.noreply.github.com>
2018-08-26 20:52:43 +0300

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -22,7 +22,21 @@ client.on('message', msg => {
 
 
 
-
+client.on('message', message => {
+    if(!message.channel.guild) return;
+if (message.content.startsWith('$ping')) {
+if(!message.channel.guild) return;
+var msg = `${Date.now() - message.createdTimestamp}`
+var api = `${Math.round(client.ping)}`
+if (message.author.bot) return;
+let embed = new Discord.RichEmbed()
+.setAuthor(message.author.username,message.author.avatarURL)
+.setColor('RANDOM')
+.addField('**Time Taken:**',msg + " ms 📶 ")
+.addField('**WebSocket:**',api + " ms 📶 ")
+message.channel.send({embed:embed});
+}
+});
 
 
 
