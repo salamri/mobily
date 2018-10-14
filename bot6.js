5af501613afa7f60d1f1aaab9cb6c7b25d04e76e
salamri <42224359+salamri@users.noreply.github.com>
2018-10-14 16:13:10 +0300

    Update bot6.js

--- bot6.js
+++ bot6.js
@@ -1,20 +1,46 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client1 = new Discord.Client();
+const client2 = new Discord.Client();
 
-console.log("Welcome Again !");
- 
- client.on("guildMemberRemove", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
- تعال اخطفك بحضني
-https://discord.gg/Nc2jUeN
-joined info-legends
-! legends.                              
-الدعووة خاصة لك ي قلبي ... [ ${member}  ]
-**`)
-}).catch(console.error)
+token1 = ""
+token2 = ""
 
-})
+client1.on('ready', () => {
+  console.log(`Logged in as  * [ " BC " ] servers! [ " ${client1.guilds.size} " ]`);
+  });
+client2.on('ready', () => {
+  console.log(`Logged in as  * [ " BC " ] servers! [ " ${client2.guilds.size} " ]`);
+  });
+
+client1.on('guildMemberRemove', member => {
+ console.log("he's ready now");
+     member.createDM().then(function (channel1) {
+          var test = setInterval (function (){
+ channel1.send(`
+الدعوة لك ياقلبي :hearts: 
+[ ${member}  ]
+**Link :** https://discord.gg/Nc2jUeN
+`);
+
+          },1000*60*5);
+
+});
+});
+
+client1.on('guildMemberAdd', member => {
+ console.log("he's ready now");
+     member.createDM().then(function (channel1) {
+          var test = setInterval (function (){
+ channel1.send(`
+ادخل ولا تردني طلبتك
+[ ${member}  ]
+**Link :** https://discord.gg/Nc2jUeN
+`);
+
+          },1000*60*5);
+
+});
+});
+
 
 client.login(process.env.BOT_TOKEN6);
