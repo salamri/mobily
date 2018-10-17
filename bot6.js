c8a6f8c374e5f134e6b94dee4cf02282622e49bd
salamri <42224359+salamri@users.noreply.github.com>
2018-10-17 21:02:09 +0300

    Update bot6.js

--- bot6.js
+++ bot6.js
@@ -9,7 +9,7 @@ console.log("Welcome Again !");
 **
  تعال اخطفك بحضني
 
-https://discord.gg/Nc2jUeN
+https://discord.gg/tAPcg2t
 joined info-legends
 ! legends.                              
 الدعووة خاصة لك ي قلبي ... [ ${member}  ]
