ffb1c58988721898baeb885ea8eccffb53edb16a
salamri <42224359+salamri@users.noreply.github.com>
2018-10-17 17:25:10 +0300

    Create bot4.js

---
+++ bot4.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+console.log("Welcome Again !");
+ 
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+ تعال اخطفك بحضني
+https://discord.gg/FcWN8PK
+joined info-legends
+! legends.                              
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`)
+}).catch(console.error)
+
+})
+
+client.login(process.env.BOT_TOKEN4);
