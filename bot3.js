b14b26f8214928a3f94705d37d805c81aa228838
salamri <42224359+salamri@users.noreply.github.com>
2018-11-08 05:17:06 +0300

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -8,14 +8,14 @@ console.log("Welcome Again !");
   return channel.send(` 
 **
  تعال اخطفك بحضني
-https://discord.gg/3PnZK7S
+
+https://discord.gg/Nc2jUeN
 joined info-legends
-! legends.                              
+! regslity.                              
 الدعووة خاصة لك ي قلبي ... [ ${member}  ]
 **`)
 }).catch(console.error)
 
 })
-
 
  client.login(process.env.BOT_TOKEN3);
