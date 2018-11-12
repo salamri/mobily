91cb74bca370d0db1f6966c06ba6bf8333e467bb
salamri <42224359+salamri@users.noreply.github.com>
2018-11-12 18:37:47 +0300

    Create bot6.js

---
+++ bot6.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+console.log("Welcome Again !");
+ 
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+ تعال اخطفك بحضني
+
+https://discord.gg/UjCGsG8
+joined info-legends
+! regslity.                              
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`)
+}).catch(console.error)
+
+})
+
+ client.login(process.env.BOT_TOKEN6);
