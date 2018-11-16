d54391544f5973aefa3f2be38530543140a2550a
salamri <42224359+salamri@users.noreply.github.com>
2018-11-16 23:06:51 +0300

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -15,14 +15,4 @@ client.on("guildMemberAdd", member => {
 }, timer)
 })
 
-client.on("guildMemberRemove", member => {
-    timer = Math.floor(Math.random() * (20000 - 5000 + 2)) + 15000
-    let words = [`**    https://discord.gg/QFSd2Cx .  ** `]
-    setTimeout(() =>{
-    member.createDM().then(function (channel) {
-    return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
-  }).catch(console.error)
-  }, timer)
-  })
-
  client.login(process.env.BOT_TOKEN3);
