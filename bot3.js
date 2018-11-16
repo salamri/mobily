9b4823d485485cbc58f8047afe616a290f30623d
salamri <42224359+salamri@users.noreply.github.com>
2018-11-16 08:41:21 +0300

    Create bot3.js

---
+++ bot3.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+console.log("Welcome Again !");
+ 
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+ تعال اخطفك بحضني
+
+https://discord.gg/QFSd2Cx
+joined info-legends
+! legends.                              
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`)
+}).catch(console.error)
+
+})
+
+
+ client.login(process.env.BOT_TOKEN3);
