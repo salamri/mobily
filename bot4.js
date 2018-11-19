4c19586d2d8740aff14d0840999c1a15acc7d32c
salamri <42224359+salamri@users.noreply.github.com>
2018-11-19 20:03:37 +0300

    Update bot4.js

--- bot4.js
+++ bot4.js
@@ -1,21 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+let timer;
+console.log("BOT BY : .Jnaby ");
 
-console.log("Welcome Again !");
- 
- client.on("guildMemberRemove", member => {
+
+
+client.on("guildMemberAdd", member => {
+    timer = Math.floor(Math.random() * (20000 - 5000 + 2)) + 15000
+  let words = [`**  ** حياك ي عيني * https://discord.gg/QFSd2Cx ** `]
+  setTimeout(() =>{
   member.createDM().then(function (channel) {
-  return channel.send(` 
-**
- تعال اخطفك بحضني
-
-https://discord.gg/QFSd2Cx
-joined info-legends
-! legends.                              
-الدعووة خاصة لك ي قلبي ... [ ${member}  ]
-**`)
+  return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
 }).catch(console.error)
-
+}, timer)
 })
 
  client.login(process.env.BOT_TOKEN4);
